815420889457a430a5eb524ad1ce62f6dd8bfcb3
invailid-expression <rohitsurwade34@gmail.com>
2024-10-07 23:02:16 +0530

    project created, class library added, folder created, dbcontext added

---
+++ InventoryManagement/Application/Entity/Model/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Entity.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public long Quantity {  get; set; }
+
+        public long Price { get; set; }
+        
+        public DateTime AddedDate { get; set; }
+
+        public long AddedBy { get; set; }
+
+        public DateTime UpdateDateTime { get; set; }
+
+        public long UpdatedBy { get; set; }
+
+        public bool IsActive { get; set; } 
+
+    }
+}
---
+++ InventoryManagement/Infrastructure/Context/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Build.Tasks.Deployment.Bootstrapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Context
+{
+    public class ApplicationContext : DbContext 
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Product { get; set; }
+    }
+}
